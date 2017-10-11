94c2e215b8252f10a42a631181582c1410ed5c0d
ismail <ismail.elshaarawy@gmail.com>
2017-10-11 12:28:52 +0200

    task1 without Author and Editor login

---
+++ administrator/logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-10-09 23:24:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-10-09T23:24:28+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2017-10-10T14:22:42+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
+2017-10-10T14:22:47+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
